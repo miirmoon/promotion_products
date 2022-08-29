470f38ecee5dad8766a05aab67881952ee50e9d3
신미래 <sml6209@naver.com>
2022-08-29 16:21:18 +0900

    [design]: 공통 스타일 정의 - 기본적인 스타일만 추가 - 추후 진행하면서 수정 예정

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    padding-top: ${(props) => props.theme.boxSize.header};
+    font-size: ${(props) => props.theme.fontSize.normal};
+  }
+
+  button {
+    border: none;
+    background-color: ${(props) => props.theme.colors.white};
+    font-family: inherit;
+    font-size: inherit;
+    cursor: pointer;
+  }
+
+  h1 {
+    font-size: ${(props) => props.theme.fontSize.large};
+    font-weight: ${(props) => props.theme.fontWeight.bold};
+  }
+
+  h2 {
+    font-size: ${(props) => props.theme.fontSize.medium};
+    font-weight: ${(props) => props.theme.fontWeight.bold};
+  }
+
+  h3 {
+    font-size: ${(props) => props.theme.fontSize.normal};
+    font-weight: ${(props) => props.theme.fontWeight.bold};
+  }
+
+  ::-webkit-scrollbar {
+    z-index: 1000;
+    width: 0.6rem;
+    height: 0.6rem;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background-color: ${(props) => props.theme.colors.grey15};
+  }
+
+  ::-webkit-scrollbar-track {
+    margin: $size-regular;
+    border-radius: 4px;
+    background-color: ${(props) => props.theme.colors.grey01};
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/styles/theme.ts
@@ -0,0 +1,70 @@
+import { DefaultTheme } from "styled-components";
+
+const colors = {
+  white: "#fff",
+  black: "#222",
+  grey01: "#f3f3f3",
+  grey05: "#e0e0e0",
+  grey10: "#9e9e9e",
+  grey15: "#767676",
+  grey20: "#575757",
+  blue: "##4774cf",
+  yellow: "#FFC61C",
+  red: "##db392e",
+};
+
+const fontSize = {
+  large: "23px",
+  medium: "17px",
+  normal: "14px",
+  small: "12px",
+  micro: "10px",
+};
+
+const fontWeight = {
+  bold: 700,
+  semiBold: 500,
+  normal: 400,
+  this: 300,
+};
+
+/** margin, padding */
+const boxSize = {
+  header: "60px",
+  big: "50px",
+  large: "25px",
+  medium: "20px",
+  normal: "16px",
+  small: "13px",
+  micro: "10px",
+};
+
+/** box-shadow shape */
+const shadow = {
+  normal: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+  thin: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+};
+
+/** device size */
+const mediaSize = {
+  minMobile: "320px",
+  mobile: "420px",
+};
+
+const theme: DefaultTheme = {
+  colors,
+  fontSize,
+  fontWeight,
+  boxSize,
+  shadow,
+  mediaSize,
+};
+
+export default theme;
+
+export type ColorsTypes = typeof colors;
+export type FontSizeTypes = typeof fontSize;
+export type FontWeightTypes = typeof fontWeight;
+export type BoxSizeTypes = typeof boxSize;
+export type ShadowTypes = typeof shadow;
+export type MediaSizeTypes = typeof mediaSize;
